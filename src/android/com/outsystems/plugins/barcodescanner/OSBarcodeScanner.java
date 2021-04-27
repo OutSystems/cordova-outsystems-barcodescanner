3128e8cea8bca4692c555b2911873fcaa0a4b764
Carlos Corrêa <carlscorrea@gmail.com>
2021-04-27 15:29:49 +0100

    feature: first implementation of ZXing library
    references: https://outsystemsrd.atlassian.net/browse/RMET-594

--- src/android/com/outsystems/plugins/barcodescanner/OSBarcodeScanner.java
+++ src/android/com/outsystems/plugins/barcodescanner/OSBarcodeScanner.java
@@ -6,6 +6,13 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class OSBarcodeScanner extends CordovaPlugin {
 
@@ -13,13 +20,15 @@ public class OSBarcodeScanner extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("scan")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.scan(message, callbackContext);
             return true;
         }
         return false;
     }
 
     private void scan(String message, CallbackContext callbackContext) {
+        new IntentIntegrator(cordova.getActivity()).initiateScan();
+
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
