92b9eaad1b10aedc345c6d7c5c4ffd795dd538d7
Carlos Corrêa <carlscorrea@gmail.com>
2021-05-07 18:45:16 +0100

    feat: custom layout first implementation
    references: https://outsystemsrd.atlassian.net/browse/RMET-594

---
+++ src/android/com/outsystems/plugins/barcodescanner/CustomScannerActivity.java
@@ -0,0 +1,140 @@
+package com.outsystems.plugins.barcodescanner;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+
+import com.outsystems.barcodescanner.R;
+
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.ViewfinderView;
+import com.journeyapps.barcodescanner.*;
+
+import java.util.Random;
+
+/**
+ * Custom Scannner Activity extending from Activity to display a custom layout form scanner view.
+ */
+public class CustomScannerActivity extends Activity implements
+        DecoratedBarcodeView.TorchListener {
+
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
+    private ImageButton switchFlashlightButton;
+    private ViewfinderView viewfinderView;
+    private boolean flashlightOn=false;
+    private boolean isScanning=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_scanner);
+
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
+        barcodeScannerView.setTorchListener(this);
+
+        switchFlashlightButton = findViewById(R.id.switch_flashlight);
+
+        viewfinderView = findViewById(R.id.zxing_viewfinder_view);
+
+        // if the device does not have flashlight in its camera,
+        // then remove the switch flashlight button...
+        if (!hasFlash()) {
+            switchFlashlightButton.setVisibility(View.GONE);
+        }
+
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.setShowMissingCameraPermissionDialog(false);
+
+        changeMaskColor(null);
+        changeLaserVisibility(true);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        capture.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        capture.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        capture.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        capture.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Check if the device's camera has a Flashlight.
+     * @return true if there is Flashlight, otherwise false.
+     */
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public void scan(View view) {
+        capture.decode();
+    }
+
+    public void closeScreen(View view) {
+        this.finish();
+    }
+
+    public void switchFlashlight(View view) {
+        if (flashlightOn) {
+            barcodeScannerView.setTorchOff();
+        } else {
+            barcodeScannerView.setTorchOn();
+        }
+    }
+
+    public void changeMaskColor(View view) {
+        int color = Color.argb(100, 0, 0, 0);
+        viewfinderView.setMaskColor(color);
+    }
+
+    public void changeLaserVisibility(boolean visible) {
+        viewfinderView.setLaserVisibility(visible);
+    }
+
+    @Override
+    public void onTorchOn() {
+        flashlightOn = true;
+    }
+
+    @Override
+    public void onTorchOff() {
+        flashlightOn = false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        capture.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
--- src/android/com/outsystems/plugins/barcodescanner/OSBarcodeScanner.java
+++ src/android/com/outsystems/plugins/barcodescanner/OSBarcodeScanner.java
@@ -1,23 +1,23 @@
 package com.outsystems.plugins.barcodescanner;
-
+import android.content.Intent;
+import android.util.Log;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
-
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-
-import org.json.JSONArray;
-import org.json.JSONException;
+import com.google.zxing.integration.android.IntentResult;
 
 public class OSBarcodeScanner extends CordovaPlugin {
 
+    public final int CUSTOMIZED_REQUEST_CODE = 0x0000ffff;
+    static private CallbackContext _callbackContext;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        _callbackContext = callbackContext;
+
         if (action.equals("scan")) {
             String message = args.getString(0);
             this.scan(message, callbackContext);
@@ -27,12 +27,40 @@ public class OSBarcodeScanner extends CordovaPlugin {
     }
 
     private void scan(String message, CallbackContext callbackContext) {
-        new IntentIntegrator(cordova.getActivity()).initiateScan();
+
+        IntentIntegrator integrator = new IntentIntegrator(this.cordova.getActivity());
+        integrator.setOrientationLocked(false);
+        integrator.setCaptureActivity(CustomScannerActivity.class);
+        integrator.initiateScan();
 
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
+        }
+
+        this.cordova.setActivityResultCallback(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != CUSTOMIZED_REQUEST_CODE && requestCode != IntentIntegrator.REQUEST_CODE) {
+            // This is important, otherwise the result will not be passed to the fragment
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+
+        if(result.getContents() == null) {
+            Intent originalIntent = result.getOriginalIntent();
+            if (originalIntent == null) {
+                _callbackContext.success("Cancelled");
+            } else if(originalIntent.hasExtra(Intents.Scan.MISSING_CAMERA_PERMISSION)) {
+                _callbackContext.success("Cancelled due to missing camera permission");
+            }
+        } else {
+            _callbackContext.success(result.getContents());
         }
     }
 
